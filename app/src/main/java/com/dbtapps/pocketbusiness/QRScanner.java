bd199059714edbd9ed5d179e53359316fc9746d8
Debarghya Basak <debarghyabasakofficial.2001@gmail.com>
2023-06-26 01:41:35 +0530

    Added QR Code scanner and permission requests

--- app/src/main/java/com/dbtapps/pocketbusiness/QRScanner.java
+++ app/src/main/java/com/dbtapps/pocketbusiness/QRScanner.java
@@ -1,14 +1,111 @@
 package com.dbtapps.pocketbusiness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class QRScanner extends AppCompatActivity {
 
+    private CodeScanner mCodeScanner;
+
+    private boolean permissionFLAG = false;
+
+    private final int PERMISSION_REQUEST_CODE = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscanner);
+
+        //INITIALIZATION----------------------------------------------------------------------------
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //INITIALIZATION END------------------------------------------------------------------------
+
+
+        checkCameraPermission();
+        Log.d("Debug", "camera permission taken");
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(QRScanner.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    private void checkCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+            permissionFLAG = true;
+        else
+            askUserPermission();
+
+    }
+
+    public void askUserPermission(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case PERMISSION_REQUEST_CODE:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    permissionFLAG = true;
+                }
+                else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    permissionFLAG = false;
+                }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(permissionFLAG){
+            Log.d("Debug", "OnResume");
+            mCodeScanner.startPreview();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        if(permissionFLAG){
+            Log.d("Debug", "OnPause");
+            mCodeScanner.releaseResources();
+        }
+        else{
+            Log.d("Debug","OnPause permissionFlag false");
+        }
+        super.onPause();
     }
 }
