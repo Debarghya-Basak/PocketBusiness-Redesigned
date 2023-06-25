9ecc48f1fb8918615f69df03f531d19ff825ac08
Debarghya Basak <debarghyabasakofficial.2001@gmail.com>
2023-06-26 02:57:26 +0530

    Item can now be successfully added with qrcode

--- app/src/main/java/com/dbtapps/pocketbusiness/SellPage.java
+++ app/src/main/java/com/dbtapps/pocketbusiness/SellPage.java
@@ -9,10 +9,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -77,9 +79,22 @@ public class SellPage extends AppCompatActivity {
 
     public void scanQRAddItemToSell(View v){
 
-        //TODO: Make scanQRAddItemToSell and delete this temporary function
         Intent intent = new Intent(SellPage.this, QRScanner.class);
-        startActivity(intent);
+
+        //ANIMATION SECTION ------------------------------------------------------------------------
+        sectionSellContainer.animate().setDuration(MainActivity.GLOBAL_FADE_ANIMATION_DURATION).alpha(0);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        },MainActivity.GLOBAL_FADE_ANIMATION_DURATION);
+        //ANIMATION SECTION END --------------------------------------------------------------------
+
+
+
 
     }
 
@@ -186,19 +201,101 @@ public class SellPage extends AppCompatActivity {
 
     }
 
+    private void startBottomDialogBoxWithQRDetails(InventoryItemModel i) {
+
+        //INITIALIZATION OF BOTTOM DIALOG BOX-------------------------------------------------------
+        LayoutInflater inflater = getLayoutInflater();
+        View bottomDialogBoxView = inflater.inflate(R.layout.add_manually_sell_page_dialog_box, null);
+
+        BottomDialogBox bottomDialogBox = new BottomDialogBox(this, bottomDialogBoxView);
+        bottomDialogBox.showDialogBox();
+
+        TextView sellPageDialogBoxName,sellPageDialogBoxCostPrice,sellPageDialogBoxSellPrice,sellPageDialogBoxQuantity,sellPageDialogBoxUnit,sellPageDialogBoxID;
+        TextInputEditText quantityEditText;
+        RecyclerView recyclerView;
+        SearchView searchView;
+
+        MaterialButton cancelButton = (MaterialButton) bottomDialogBoxView.findViewById(R.id.cancelButton_addManuallySellPage);
+        MaterialButton addButton = (MaterialButton) bottomDialogBoxView.findViewById(R.id.addButton_addManuallySellPage);
+
+        recyclerView = (RecyclerView) bottomDialogBoxView.findViewById(R.id.recyclerView_SellPageBottomDialogBox_addManually);
+
+        searchView = (SearchView) bottomDialogBoxView.findViewById(R.id.searchView_BottomDialogBox_SellPage);
+
+        sellPageDialogBoxName = bottomDialogBoxView.findViewById(R.id.sellPage_BottomDialogBox_ShowName);
+        sellPageDialogBoxCostPrice = bottomDialogBoxView.findViewById(R.id.sellPage_BottomDialogBox_ShowCostPrice);
+        sellPageDialogBoxSellPrice = bottomDialogBoxView.findViewById(R.id.sellPage_BottomDialogBox_ShowSellPrice);
+        sellPageDialogBoxQuantity = bottomDialogBoxView.findViewById(R.id.sellPage_BottomDialogBox_ShowQuantity);
+        sellPageDialogBoxUnit = bottomDialogBoxView.findViewById(R.id.sellPage_BottomDialogBox_ShowUnit);
+        sellPageDialogBoxID = bottomDialogBoxView.findViewById(R.id.sellPage_BottomDialogBox_ShowID);
+
+        quantityEditText = bottomDialogBoxView.findViewById(R.id.enterQuantitySellPageDialogBox);
+        Log.d("Debug", "SellPage: Bottom dialog box layout initialization done");
+        //INITIALIZATION END------------------------------------------------------------------------
+
+        //PROVIDING VALUES--------------------------------------------------------------------------
+        ViewGroup.LayoutParams recyclerViewLayoutEdit = recyclerView.getLayoutParams();
+        recyclerViewLayoutEdit.height = 0;
+        recyclerView.setLayoutParams(recyclerViewLayoutEdit);
+
+        ViewGroup.LayoutParams searchViewLayoutEdit = searchView.getLayoutParams();
+        searchViewLayoutEdit.height = 0;
+        searchView.setLayoutParams(searchViewLayoutEdit);
+
+        sellPageDialogBoxName.setText(i.name);
+        sellPageDialogBoxCostPrice.setText((i.cost_price.size()-1) + "");
+        sellPageDialogBoxSellPrice.setText(i.sell_price + "");
+        sellPageDialogBoxQuantity.setText((i.cost_price.size()-1) + "");
+        sellPageDialogBoxUnit.setText(i.unit+"");
+        sellPageDialogBoxID.setText(i.id+"");
+        Log.d("Debug", "SellPage: Values given to bottom dialog box");
+        //PROVIDING VALUES END----------------------------------------------------------------------
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomDialogBox.dismissDialogBox();
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!TextUtils.isEmpty(quantityEditText.getText().toString())) {
+                    sellList.add(i);
+                    sellListQuantityUnit.add(new SellListQuantityUnitModel(Integer.parseInt(quantityEditText.getText().toString()), i.unit + ""));
+                    reloadSellPageAdapter();
+                    bottomDialogBox.dismissDialogBox();
+                }
+
+
+            }
+        });
+
+    }
+
+    public void startBillPage(View v){
+        Toast.makeText(this, "Start Bill Page" , Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("Debug","SellPage: On Resume");
+
+        //ANIMATION SECTION ------------------------------------------------------------------------
+        sectionSellContainer.setAlpha(0);
+        sectionSellContainer.animate().setDuration(MainActivity.GLOBAL_FADE_ANIMATION_DURATION).alpha(1);
+        //ANIMATION SECTION END --------------------------------------------------------------------
+
         if(QRCODE != -1){
             boolean isFoundFlag = false;
             for(InventoryItemModel i : LoadInventoryData.inventoryItems){
                 if(i.id == QRCODE) {
                     isFoundFlag = true;
-                    sellList.add(i);
-                    //TODO: ADD DIALOG BOX TO ACCEPT QUANTITY FROM USER and then add to sellListQuantityUnit
-                    sellListQuantityUnit.add(new SellListQuantityUnitModel(10, "Kg"));
-                    reloadSellPageAdapter();
+                    startBottomDialogBoxWithQRDetails(i);
+                    //sellListQuantityUnit.add(new SellListQuantityUnitModel(10, "Kg"));
                     QRCODE = -1;
                 }
             }
@@ -207,4 +304,6 @@ public class SellPage extends AppCompatActivity {
         }
 
     }
+
+
 }
